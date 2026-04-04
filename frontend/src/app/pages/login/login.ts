8f381bf5daeb629f17356a13d21689ba4c19ddeb
Mohamed Elgendy <moellgendy@gmail.com>
2026-04-04 14:47:55 +0200

    implement login component

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { Auth } from '../../core/services/auth';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  constructor(private auth:Auth,private router:Router ){}
+  get f(){
+    return this.form.controls;
+  }
+  form=new FormGroup({
+    email:new FormControl('',[Validators.required,Validators.email]),
+    password:new FormControl('',[Validators.required,Validators.minLength(6)])
+  })
+  error='';
+  loading=false;
+  submit(){
+    if(this.form.invalid) return;
+    this.loading=true;
+    this.error='';
+
+    const {email,password}=this.form.value;
+    this.auth.login({email:email!,passowrd:password!}).subscribe({
+      next:()=>{
+        this.loading=false;
+        this.router.navigate(['/products']);
+      },
+      error:(err)=>{
+        this.loading=false;
+        this.error=err.error.message || 'Login failed';
+      }
+    })
+    
+  }
+}
