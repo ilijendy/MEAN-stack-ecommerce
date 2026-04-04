17f98d6440548b8236e1e11053ab53882c50d8de
Mohamed Elgendy <moellgendy@gmail.com>
2026-04-04 15:05:11 +0200

    implement  admin and auth guards

--- frontend/src/app/core/guards/admin-guard.ts
+++ frontend/src/app/core/guards/admin-guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { Auth } from '../services/auth';
+import { inject } from '@angular/core';
 
 export const adminGuard: CanActivateFn = (route, state) => {
-  return true;
+  const auth=inject(Auth);
+  const router=inject(Router)
+
+  if(auth.isLogIn()&&auth.isAdmin()){
+    return true;
+  }else{
+    router.navigate(['/login']);
+    return false;
+  }
 };
